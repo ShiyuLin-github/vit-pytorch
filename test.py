9c54835fcc9cd83a2d10c6bf2fa05fafa5d3a410
ShiyuLin-github <1499901128@qq.com>
2024-01-09 15:39:34 +0800

    111

--- test.py
+++ test.py
@@ -21,3 +21,21 @@ video = torch.randn(4, 1, 154, 240, 240) # (batch, channels, frames, height, wid
 preds = v(video) # (4, 3)
 
 print(preds)
+
+
+import torch
+from vit_pytorch import SimpleViT
+
+v = SimpleViT(
+    image_size = 256,
+    patch_size = 32,
+    num_classes = 1000,
+    dim = 1024,
+    depth = 6,
+    heads = 16,
+    mlp_dim = 2048
+)
+
+img = torch.randn(1, 3, 256, 256)
+
+preds = v(img) # (1, 1000)
